3defbc269376448aa03be238d520fb015d46415d
Kanha Jain <8110159-KanhaJain99@users.noreply.gitlab.com>
2021-03-10 15:26:25 +0530

    404 problem

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+const notFound = () => {
+  return (
+    <div style={{ display: "grid", placeItems: "center", height: "100vh" }}>
+      <div>
+        <img src="vercel.svg" />
+        <br />
+        <h2 style={{ textAlign: "center" }}>Not Found</h2>
+      </div>
+    </div>
+  );
+};
+
+export default notFound;
